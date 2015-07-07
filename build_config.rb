678076649746dfc1e7611b95fe67220ab7483989
Zachary Scott <e@zzak.io>
2015-07-07 15:29:11 -0400

    Update dependencies

--- build_config.rb
+++ build_config.rb
@@ -1,9 +1,10 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
 
-  conf.gembox 'default'
-  conf.gem :mgem => 'mruby-onig-regexp'
-  conf.gem :mgem => 'mruby-mtest'
+  #conf.gembox 'default'
+  conf.gem mgem: 'mruby-onig-regexp'
+  conf.gem mgem: 'mruby-mtest'
+  conf.gem mgem: 'mruby-io'
 
   conf.gem File.expand_path(File.dirname(__FILE__))
 end
