0ef43bb94c97b408929548aa576ba4cc48fea0b7
Zachary Scott <e@zzak.io>
2015-07-09 16:52:25 -0400

    Add mruby-string-ext as dependency

--- build_config.rb
+++ build_config.rb
@@ -1,6 +1,7 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
 
+  conf.gem :core => 'mruby-string-ext'
   conf.gem :mgem => 'mruby-onig-regexp'
   conf.gem :mgem => 'mruby-io'
   conf.gem :mgem => 'mruby-mtest'
