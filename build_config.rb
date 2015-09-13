65cda4870716d4270438e3293788091973a3909a
Zachary Scott <e@zzak.io>
2015-09-13 08:47:41 -0400

    Remove duplicate dependencies from build_config

--- build_config.rb
+++ build_config.rb
@@ -1,10 +1,5 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
 
-  conf.gem :core => 'mruby-string-ext'
-  conf.gem :mgem => 'mruby-onig-regexp'
-  conf.gem :mgem => 'mruby-io'
-  conf.gem :mgem => 'mruby-mtest'
-
   conf.gem File.expand_path(File.dirname(__FILE__))
 end
